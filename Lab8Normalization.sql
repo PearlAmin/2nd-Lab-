67feb7e49c964edb80c7ed6fd438e855353eca5f
PearlAmin <31822472+PearlAmin@users.noreply.github.com>
2017-11-13 01:02:25 -0500

    Add files via upload
    I lost a piece of soul doing this. 
    Lab 8
    Normalization 2 
    Nov 13th, 2017 
    RIP

---
+++ Lab8Normalization.sql
@@ -0,0 +1,110 @@
+﻿-- Pearl Amin
+-- Lab 8 Normalization 2
+-- Nov 13th, 2017
+-- Database Systems  
+
+DROP TABLE IF EXISTS People CASCADE;
+DROP TABLE IF EXISTS Actors CASCADE;
+DROP TABLE IF EXISTS Spouses CASCADE;
+DROP TABLE IF EXISTS Movies CASCADE ;
+DROP TABLE IF EXISTS Directors CASCADE;
+DROP TABLE IF EXISTS MovieActors CASCADE;
+DROP TABLE IF EXISTS MovieDirectors CASCADE;
+DROP TABLE IF EXISTS ZipState CASCADE;
+DROP TABLE IF EXISTS ZipCity CASCADE ;
+DROP TABLE IF EXISTS StateCountry CASCADE;
+
+
+-- Zip -- 
+CREATE TABLE ZipState (ZipStateID    	integer    PRIMARY KEY,
+					   ZipCode			real	not null,
+					   State			text
+					 );
+					 
+-- StateCountry -- 
+CREATE TABLE StateCountry (StateCountryID	integer  PRIMARY KEY, 
+						   StateName		text, 
+						   CountryName      text
+						  ); 
+						  
+-- Zip City -- 
+CREATE TABLE ZipCity (ZipCityID             integer   PRIMARY KEY, 
+				         ZipCode			int, 
+				         CityName			text
+				        ); 
+-- People -- 
+CREATE TABLE People (	
+					 PID			        integer	PRIMARY KEY,
+					 FirstName				text	not null,
+					 LastName				text,
+					 StreetAddress       	varchar	not null,
+					 ZipStateID	       		int	  	not null	 references ZipState(ZipStateID),
+					 ZipCityID				int   	not null    references ZipCity(ZipCityID),
+					 StateCountryID			int 	not null    references StateCountry(StateCountryID),
+					 Married		        bool, 
+					 Actor					bool,
+					 Director 		        bool 
+					);
+
+
+-- Actors --
+CREATE TABLE Actors (
+					 PID				int		not null	references People(PID),
+					 Birthday 			int, 
+					 HairColour			text,
+					 EyeColour			text,
+					 HeightInch			int,
+					 WeightLBS			int,
+					 FavColour			text,
+					 SAGuildAnnivDate	int,
+				         primary key(PID)
+					); 
+				
+-- Spouses -- 
+CREATE TABLE Spouses (
+					   PID					int		not null	unique	references People(PID),
+					   SpousesFN		text,
+					   SpousesLN		text,
+					primary key(PID) 
+					 ); 
+					 
+-- Movies -- 
+CREATE TABLE Movies(
+					MovieID					int		PRIMARY KEY,
+					Name					text	not null,
+					YearReleased			        int,
+					MPAANumber				int,
+					DomestBoxSalesUSD							numeric(9,0),
+					DVDBluRaySales							    numeric(9,0),
+					ForeignBoxSalesUSD							numeric(9,0)
+				    );
+				
+-- Directors -- 
+CREATE TABLE Directors(
+					   PID					int	references People(PID),
+					   FilmSchool			text,
+					   SAGuildAnnivDate		int,
+					   FavLenseMaker 		text, 
+					primary key(PID) 
+					   ); 
+					 
+-- Movie Directors -- 
+CREATE TABLE MovieDirectors (
+						   	MovieID			int		not null	references Movies(MovieID),
+						     DirectorPID	int 	not null	references Directors(PID)
+    						);
+                            
+    						
+-- Movie Actors -- 
+CREATE TABLE MovieActors (
+							MovieID int	not null	references Movies(MovieID),
+							ActorPID int not null references Actors(PID)
+							);
+                   
+                   
+                   
+
+
+
+
+    
---
+++ lab 8 normalization inserts.sql
@@ -0,0 +1,122 @@
+﻿                   
+ -- SAMPLE DATA FOR THE ABOVE --
+
+						
+-- State Country -- 
+insert into StateCountry(StateCountryID, StateName, CountryName) values
+						(08807, 'California', 'USA'); 		
+insert into StateCountry(StateCountryID, StateName, CountryName) values
+						(08808, 'Idaho', 'USA'); 						
+insert into StateCountry(StateCountryID, StateName, CountryName) values
+						(08800, 'Penn', 'USA'); 						
+insert into StateCountry(StateCountryID, StateName, CountryName) values
+						(08801, 'Maine', 'USA'); 
+						
+-- Zip State -- 
+insert into ZipState (ZipStateID, ZipCode, State) values 
+					(18809, 01, 'California') ;	
+insert into ZipState (ZipStateID, ZipCode, State) values 
+					(18802, 01, 'California') ;							
+insert into ZipState (ZipStateID, ZipCode, State) values 
+					(18803, 01, 'California') ;	
+insert into ZipState (ZipStateID, ZipCode, State) values 
+					(18804, 01, 'California') ;	
+					
+-- ZipCity -- 	
+insert into ZipCity(ZipCityID, ZipCode, CityName) values
+					(01, 01,'Los Angeles') ;
+insert into ZipCity(ZipCityID, ZipCode, CityName) values
+					(02, 01,'Los Angeles') ;					
+insert into ZipCity(ZipCityID, ZipCode, CityName) values
+					(03, 01,'Los Angeles') ;					
+insert into ZipCity(ZipCityID, ZipCode, CityName) values
+					(04, 01,'Los Angeles') ;	
+                    
+-- People --
+insert into People(pid, FirstName, LastName, StreetAddress, ZipStateID, ZipCityID, StateCountryID, Married, Actor, Director) values 
+					(9, 'Roger', 'Moore', '131 Rodeo Drive', 18802, '01', '08807', true, true, false);
+insert into People(PID, FirstName, LastName, StreetAddress, ZipStateID, ZipCityID, StateCountryID, Married, Actor, Director) values 
+					(8, 'Not', 'Me', '121 Rodeo Drive', 18803, '02', '08807', true, false, true);
+insert into People(PID, FirstName, LastName, StreetAddress, ZipStateID, ZipCityID, StateCountryID, Married, Actor, Director) values 
+					(7, 'Actor', 'Guy', '111 Rodeo Drive', 18804, '03', '08807', true, true, false);
+insert into People(PID, FirstName, LastName, StreetAddress, ZipStateID, ZipCityID, StateCountryID, Married, Actor, Director) values 
+					(6, 'Actor', 'Girl', '101 Rodeo Drive', 18809, '04', '08807', true, false, true);
+					
+-- Actors -- 
+insert into Actors(PID, Birthday, HairColour, EyeColour, HeightInch, WeightLBS, FavColour, SAGuildAnnivDate) values
+					(9, 04301996, 'brown', 'green', 65, 105, 'pink', 1212012) ;
+insert into Actors(PID, Birthday, HairColour, EyeColour, HeightInch, WeightLBS, FavColour, SAGuildAnnivDate) values
+					(7, 05301996, 'black', 'blue', 65, 106, 'blue', 1212012) ;
+
+
+
+-- Directors -- 
+insert into Directors(PID, FilmSchool, SAGuildAnnivDate, FavLenseMaker) values
+						(8, 'Marist', 1212012, 'Alan') ;
+
+insert into Directors(PID, FilmSchool, SAGuildAnnivDate, FavLenseMaker) values
+						(6, 'SUNY', 1212014, 'Dennis') ;
+
+														
+-- Movies --
+insert into Movies(MovieID, Name, YearReleased, MPAANumber, DomestBoxSalesUSD, DVDBluRaySales, ForeignBoxSalesUSD) values 
+					(123, 'Bond', 2010, 112, 100000, 500, 900000);
+insert into Movies(MovieID, Name, YearReleased, MPAANumber, DomestBoxSalesUSD, DVDBluRaySales, ForeignBoxSalesUSD) values 
+					(345, 'James', 2011, 113, 200000, 5000, 700000);
+insert into Movies(MovieID, Name, YearReleased, MPAANumber, DomestBoxSalesUSD, DVDBluRaySales, ForeignBoxSalesUSD) values 
+					(567, 'JamesBond', 2014, 114, 300000, 50000, 800000);
+insert into Movies(MovieID, Name, YearReleased, MPAANumber, DomestBoxSalesUSD, DVDBluRaySales, ForeignBoxSalesUSD) values 
+					(789, 'JamesBondie', 2015, 115, 400000, 500000, 990000);
+					
+-- Spouses --
+insert into Spouses(PID, SpousesFN, SpousesLN) values
+					(9, 'Plain', 'Jane'); 
+insert into Spouses(PID, SpousesFN, SpousesLN) values
+					(8, 'Champagne', 'Puppa'); 			
+insert into Spouses(PID, SpousesFN, SpousesLN) values
+					(7, 'Post', 'Malone'); 	
+insert into Spouses(PID, SpousesFN, SpousesLN) values
+					(6, 'Rock', 'Star'); 				
+					
+-- Movie Actors --
+insert into MovieActors(ActorPID,MovieID) values
+						(9,789) ; 
+insert into MovieActors(ActorPID,MovieID) values
+						(7,345) ; 						
+insert into MovieActors(ActorPID,MovieID) values
+						(9,567) ; 						
+						
+						
+-- Movie Directors -- 	
+
+insert into MovieDirectors(MovieID, DirectorPID) values 
+							(345, 8) ;							
+					
+insert into MovieDirectors(MovieID, DirectorPID) values 
+							(789, 6) ;	
+							
+-------------------------------------------------
+							
+-- SQL QUERY --                    
+Select FirstName, LastName
+From People
+Where PID in ( 
+				SELECT DirectorPID
+				FROM MovieDirectors 
+									WHERE MovieId in (
+									Select MovieID 
+									From MovieActors
+									where ActorPid in (
+														Select PID 
+                                        				From People 
+														Where FirstName = 'Roger' 
+                                       				and LastName = 'Moore'))) 
+AND director = true ;
+
+-----------------------------------------------------
+
+﻿--Select *
+--From MovieActors, MovieDirectors 
+--Where movieactors.movieid = moviedirectors.movieid ; 
+
+                            
